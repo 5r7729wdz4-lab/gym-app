951c1e6cff72c6eb0f3fb04df2d077a151138f7e
5r7729wdz4-lab <5r7729wdz4-lab@users.noreply.github.com>
2026-09-07 01:39:38 +0300

    v3: network-first SW, version marker, timer fixes

--- sw.js
+++ sw.js
@@ -1,4 +1,11 @@
-var C='gym-v1';
+var C='gym-v3';
 self.addEventListener('install',function(e){e.waitUntil(caches.open(C).then(function(c){return c.addAll(['./','./index.html'])}).then(function(){return self.skipWaiting()}))});
 self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(ks){return Promise.all(ks.filter(function(k){return k!==C}).map(function(k){return caches.delete(k)}))}).then(function(){return self.clients.claim()}))});
-self.addEventListener('fetch',function(e){e.respondWith(caches.match(e.request,{ignoreSearch:true}).then(function(r){return r||fetch(e.request).then(function(res){var cl=res.clone();caches.open(C).then(function(c){c.put(e.request,cl)});return res}).catch(function(){return caches.match('./')})}))});
+self.addEventListener('fetch',function(e){
+  var req=e.request;
+  if(req.mode==='navigate'||(req.headers.get('accept')||'').indexOf('text/html')>-1){
+    e.respondWith(fetch(req).then(function(res){var cl=res.clone();caches.open(C).then(function(c){c.put('./index.html',cl)});return res}).catch(function(){return caches.match('./index.html')}));
+    return;
+  }
+  e.respondWith(caches.match(req,{ignoreSearch:true}).then(function(r){return r||fetch(req).then(function(res){var cl=res.clone();caches.open(C).then(function(c){c.put(req,cl)});return res})}));
+});
